4d7c455dbbed28fa50d64e611f412c06abf06912
Samson Poon <samsonpooon@gmail.com>
2023-03-19 21:04:49 -0500

    Updates

---
+++ precache-manifest.d8c8942eb00fc746485d2f38b94a36f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a85a74531ee85c6769e5288617bf360",
+    "url": "/spoonfoundfood/index.html"
+  },
+  {
+    "revision": "c8c0107c0a5a09b95229",
+    "url": "/spoonfoundfood/static/js/2.31a6fee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spoonfoundfood/static/js/2.31a6fee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f069261b3a76b2036261",
+    "url": "/spoonfoundfood/static/js/main.31d3003c.chunk.js"
+  },
+  {
+    "revision": "19caa522d953fa3b4679",
+    "url": "/spoonfoundfood/static/js/runtime-main.093e764d.js"
+  }
+]);
