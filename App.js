cf84d94c692472acb47a4cfed51609e9aa629244
samsonpooon <62966925+samsonpooon@users.noreply.github.com>
2023-07-28 22:45:17 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import Navbar from './components/Navbar'
+//import MainContent from './components/MainContent'
+//import Footer from './components/Footer'
+//import TodoItem from './components/TodoItem'
+
+function App() {
+	//const firstName = "Steve";
+	return (
+		<div>
+		
+		</div>
+	)
+}
+
+export default App
